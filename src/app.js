24f1994aaf3798189406cdb8efde53fa4def53dd
vargattj <vargattjh@gmail.com>
2020-07-05 18:08:35 -0300

    list of service providers

--- src/app.js
+++ src/app.js
@@ -1,23 +1,29 @@
 import express from "express";
-import routes from "./routes";
+import path from "path";
 
+import routes from "./routes";
 import "./database";
 
 class App {
   constructor() {
     this.server = express();
+
     this.middlewares();
     this.routes();
   }
 
   middlewares() {
     this.server.use(express.json());
+    this.server.use(
+      "/files",
+      express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
+    );
   }
   routes() {
     this.server.use(routes);
   }
 }
-
+ 
 export default new App().server;
 
 // Yarn Add express sequelize pg pg-hstore
