72a61f4a1e1d5b2b5cd0af636e0df792f961ca77
vargattj <vargattjh@gmail.com>
2020-07-08 12:22:31 -0300

    nodemailer and template emails

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,6 +6,8 @@ import Notification from "../schemas/Notification";
 import User from "../models/User";
 import File from "../models/File";
 
+import Mail from "../../lib/Mail";
+
 class AppointmentController {
   async index(req, res) {
     const { page = 1 } = req.query;
@@ -109,7 +111,21 @@ class AppointmentController {
   }
 
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.params.id);
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: "provider",
+          attributes: ["name", "email"],
+        },
+        {
+          model: User,
+          as: "user",
+          attributes: ["name"],
+        },
+      ],
+    });
+
     if (appointment.user_id !== req.userId) {
       return res.status(401).json({
         error: "You don't have permission to cancel this appointment",
@@ -118,15 +134,26 @@ class AppointmentController {
     const dateWithSub = subHours(appointment.date, 2);
 
     if (isBefore(dateWithSub, new Date())) {
-      return res
-        .status(401)
-        .json({
-          error: "You can only cancel appointments 2 hours in advance.",
-        });
+      return res.status(401).json({
+        error: "You can only cancel appointments 2 hours in advance.",
+      });
     }
     appointment.canceled_at = new Date();
 
     await appointment.save();
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>}`,
+      subject: "Agendamento Cancelado",
+      template: "cancellation",
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date, "'dia' dd 'de' MMMM', às' H:mm'h'", {
+          locale: pt,
+        }),
+      },
+    });
 
     return res.json(appointment);
   }
---
+++ src/config/mail.js
@@ -0,0 +1,18 @@
+export default {
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "6b8868bd3e0e92",
+    pass: "9e96103d020b28",
+  },
+  default: {
+    from: "Equipe Gobarber <noreply@gobarber.com>",
+  },
+};
+
+// Amazon SES *
+// Mailgun
+// sparkpost
+// Mandril (Maildchimp)
+
+// Mailtrap (Dev)
